54cae18287ccf8454e2f53ef10c29d51ecffc0e4
AmreetNanda <amreetnanda321@gmail.com>
2025-12-28 11:01:21 +0530

    Logger added

---
+++ logger.py
@@ -0,0 +1,23 @@
+import logging
+import os
+from datetime import datetime
+
+# Create Logs directory
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Log file path
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+# Removes previous handlers 
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO,
+)
+
+print("Log file created at: {LOG_FILE_PATH}")
